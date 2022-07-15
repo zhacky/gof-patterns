e84986a532031603de1338bf5781caff1c55b9af
GitHub Action <action@github.com>
2022-07-15 16:08:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhacky/gofpatterns/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zhacky/gofpatterns/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhacky.gofpatterns.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhacky.gofpatterns.MyBundle
 
 class MyProjectService(project: Project) {
 
